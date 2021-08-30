2bfebf8bc74b363e6103d61df705ffffaaa10cef
DerekMalone <laxmalone@gmail.com>
2021-08-29 20:15:25 -0500

    rendering the purchasing agents to the DOM

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -13,9 +13,10 @@ const greetingHeader = () => {
   <div class="business-box" id="business-container">
   <div class="accounts-container">
   <h2>Active Accounts</h2>
-  <button type="button" id="filter-btn">Filter</button>
+  <button type="button" class="filter-btn" id="filter-btn">Filter</button>
   </div>
   <div id="accounts-container"></div>
+  <div id="ny-accounts-container"></div>
   <div id="agents-container"></div>
   </div>
   `;
@@ -39,67 +40,57 @@ const activeAcounts = (array) => {
   });
 };
 
-// const filtereredBusinesses = (array) => {
-//   const locationsInNY = array.filter((arr) => arr.addressStateCode === 'NY');
-//   console.warn(locationsInNY);
-//   activeAcounts(locationsInNY);
-// };
-
-const agents = businesses.map((business) => business.purchasingAgent);
-
-agents.forEach((agent) => {
-  let domString = '';
-  domString += `
+const onlyAgents = (array) => {
+  const agents = array.map((business) => business.purchasingAgent);
+  agents.forEach((agent) => {
+    let domString = '';
+    domString += `
     <div>
-      <div> 
-        <h2>Purchasing Agent:</h2>
-      </div>
-      <div>${agent.nameFirst} ${agent.nameLast}</div>
+    <div>
+    <h2>Purchasing Agent:</h2>
+    </div>
+    <div>${agent.nameFirst} ${agent.nameLast}</div>
     </div>
     `;
-  renderToDom('#agents-container', domString);
-});
+    renderToDom('#agents-container', domString);
+  });
+};
 
-// const searchBuisnessForm = () => {
-//   console.warn('Clicked');
-//   const domString = `
-//   <div class="input-group mb-3">
-//   <label for="filter-input-field">Search:</label>
-//   <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
-//   <input type="text" class="form-control" id="filter-input-field" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-//   </div>
-//   `;
-//   renderToDom('#business-container', domString);
-//   filtereredBusinesses();
-// };
+const nyBusinesses = (array) => {
+  let domString = '';
+  array.forEach((element, i) => {
+    domString += `
+    <div>
+    <h3>NY Businesses</h3>
+     <div class="biz-container">
+      <h3 id="biz-name">${element.companyName}</h3>
+     </div>
+     <div id="biz-street">${element.addressFullStreet}</div>
+     <div id="biz-city-state-zip">${element.addressCity}, ${element.addressStateCode} ${element.addressZipCode}</div>
+     <button type="button" id="${i}"></button>
+    </div>
+    `;
+    renderToDom('#ny-accounts-container', domString);
+  });
+};
 
-// const currentBusinesses = (array) => {
-//   let element = '';
-//   array.forEach((taco) => {
-//     element = `<div class="card" style="width: 18rem;">
-//     <div class="card-body">
-//     <h5 class="card-title">${taco.companyName}</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//     </div>
-//     </div>
-//     `;
-//     renderToDom('#rendered-card', element);
-//   });
-// };
+const filtereredBusinesses = (array) => {
+  const locationsInNY = array.filter((arr) => arr.addressStateCode === 'NY');
+  nyBusinesses(locationsInNY);
+};
 
 const domEvents = () => {
-  // document.querySelector('#filter-btn').addEventListener('click', filtereredBusinesses);
+  // document.querySelector('#filter-btn').addEventListener('click', filtereredBusinesses(array));
   // document.querySelector().addEventListener();
 };
 
 const init = () => {
   greetingHeader();
   domEvents();
-  // currentBusinesses(businesses);
   activeAcounts(businesses);
-  // filtereredBusinesses(businesses);
-  agents();
+
+  filtereredBusinesses(businesses);
+  onlyAgents(businesses);
 };
 
 init();
